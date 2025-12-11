5f373e1f85786b7416847b1eb6e831ccdf168bb1
javiervera123 <veraadsi123@gmail.com>
2025-12-11 15:34:17 -0500

    de nuevo una corrección en la ruta productos controller por una mayúscula

--- controllers/productos_controller.js
+++ controllers/productos_controller.js
@@ -1,7 +1,7 @@
 // controllers/productos_controller.js
 
 // Importa el modelo de producto. Asegúrate que el nombre de archivo es 'Producto_model'
-const Producto = require('../models/Producto');
+const Producto = require('../models/producto');
 
 /**
  * Función para obtener y listar todos los productos
