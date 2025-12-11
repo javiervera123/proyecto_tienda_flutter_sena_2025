5cc2f1a51e76ab459b74929c30673a52d2351b5f
javiervera123 <veraadsi123@gmail.com>
2025-12-11 15:21:03 -0500

    corrección en password

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,                  //"localhost",  // cuando la bd es local
     user: process.env.MYSQLUSER,                  //"root",
-    password: password: process.env.MYSQLPASSWORD,  //"root"
+    password: process.env.MYSQLPASSWORD,  //"root"
     database: process.env.MYSQLDATABASE,            // "db_tienda_online",
-    port: process.env.MYSQLPORT                     //3306,
+    port: process.env.MYSQLPORT,                    //3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
