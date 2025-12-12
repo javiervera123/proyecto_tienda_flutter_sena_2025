1bdd3a816669c98bd596c75fdc7db7a94f2019d4
javiervera123 <veraadsi123@gmail.com>
2025-12-11 20:28:46 -0500

    configur primero una unica rut, luego las var como soporte

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,                  //"localhost",  // cuando la bd es local
-    user: process.env.MYSQLUSER,                  //"root",
-    password: process.env.MYSQLPASSWORD,  //"root"
-    database: process.env.MYSQLDATABASE,            // "db_tienda_online",
-    port: process.env.MYSQLPORT,                    //3306,
+// 1. Definimos la URL de conexión principal que debe usar Railway
+const connectionUrl = process.env.MYSQL_URL;
+
+const pool = mysql.createPool(connectionUrl ? connectionUrl : {
+    // 2. Si la URL falla (o no está definida), usamos los parámetros separados (como respaldo)
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
