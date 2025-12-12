830cc38f0de27c895cc5f95976837653688c349b
javiervera123 <veraadsi123@gmail.com>
2025-12-11 22:17:01 -0500

    intento 101 conexion con bd

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,8 @@
 const mysql = require("mysql2/promise");
 
-// 1. Definimos la URL de conexión principal que debe usar Railway
-const connectionUrl = "mysql://root:nJptHEktCgWeWmZKabkNfMYuQDOBUvsm@mysql.railway.internal:3306/railway";
-//
-const pool = mysql.createPool(connectionUrl ? connectionUrl : {
-    // Si MYSQL_URL existe, úsala. Si no existe, usa los 5 parámetros individuales.
+// Forzamos el uso de los 5 parámetros individuales (eliminamos la lógica del connectionUrl)
+const pool = mysql.createPool({
+    // Usaremos las variables de entorno para una carga limpia
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
