8547a39f13efc1ddf86f11e2207a2ce7ba2d360f
javiervera123 <veraadsi123@gmail.com>
2025-12-11 19:51:32 -0500

    Esto es una prueba de conexión con railway

--- lib/services/categorias_service.dart
+++ lib/services/categorias_service.dart
@@ -7,7 +7,7 @@ class CategoriasService {
   // Nota: Si estás en Android Studio o VSCode, 10.0.2.2 es correcto para localhost.
   // si es en celular la ip config del equipo.
   // en este caso el backend está en railway, un servidor en la nube
-  static const String baseUrl = "https://proyectotiendafluttersena2025-production.up.railway.app";
+  static const String baseUrl ="https://proyectotiendafluttersena2025-production.up.railway.app";
 
   static Future<List<Categoria>> getCategorias() async {
     try {
@@ -19,6 +19,7 @@ class CategoriasService {
       if (res.statusCode == 200) {
         List data = json.decode(res.body);
         return data.map((c) => Categoria.fromJson(c)).toList();
+
       } else {
         throw Exception("Error cargando categorías");
       }
